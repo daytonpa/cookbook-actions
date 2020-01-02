fbd9ceb1c8a754e5aa4de63c656cf35b61de636c
Patrick Dayton <daytonpa@gmail.com>
2020-01-02 13:35:59 -0500

    Added more functionality into the entrypoint file, and updated the actions.yml accordingly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,73 @@
 
 set -ou
 
-function main {
-  local scripts_directory="$(dirname ${0})/scripts"
-  local scripts=( chef_exec_cookstyle chef_exec_foodcritic chef_exec_rspec )
-  for script in scripts; do
-    source ${scripts_directory}/${script}.sh
-  done
+### VARIABLES ###
+#
+# EXTRA PACKAGES
+EXTRA_PACKAGES=( ca-certificates jq vim wget )
+
+function has_prefix {
+  case ${2} in
+    "${1}"*)
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
 }
 
-main
+function parse_inputs {
+  # REQUIRED
+  if [[ "${INPUT_CHEF_ACTIONS_SUBCOMMAND}" != '' ]]; then
+    chef_subcommand=${INPUT_CHEF_ACTIONS_SUBCOMMAND}
+  else
+    echo "ERROR: Input chef_actions_subcommand cannot be empty"
+    exit 1
+  fi
+
+  # OPTIONAL
+  workspace='.'
+  if [[ "${INPUT_CHEF_ACTIONS_WORKSPACE}" != '' ]] || [[ "${INPUT_CHEF_ACTIONS_WORKSPACE}" != '.' ]]; then
+    echo "INFO: Workspace is being changed from the default setting to ${INPUT_CHEF_ACTIONS_WORKSPACE}"
+    workspace="${INPUT_CHEF_ACTIONS_WORKSPACE}"
+  else
+    echo "INFO: Workspace has not been set.  Using default setting"
+  fi
+
+}
+
+function pre_run {
+  apt update -y && \
+    apt install -y \
+    ${EXTRA_PACKAGES[@]}
+}
+
+function main {
+  local scripts_directory="$(dirname ${0})/scripts"
+  local scripts_arr=($(ls -d ${scripts_directory}/*))
+
+  # Load our scripts
+  for script in scripts_arr; do
+    source ${scripts_directory}/${script}.sh
+  done
+
+  cd ${GITHUB_WORKSPACE}/${workspace}
+  case "${chef_subcommand}" in
+    cookstyle)
+      chef_exec_cookstyle ${*}
+      ;;
+    foodcritic)
+      chef_exec_foodcritic ${*}
+      ;;
+    chefspec)
+      chef_exec_rspec ${*}
+      ;;
+    *)
+      echo "ERROR: You must provide a valid value for 'chef_subcommand'"
+      exit 1
+      ;;
+  esac
+}
+
+pre_run && main "${*}"
