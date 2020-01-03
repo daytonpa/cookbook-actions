00b22785f88bc7b73f5cc31bf5be6e2776abd92c
Patrick Dayton <daytonpa@gmail.com>
2020-01-03 11:40:35 -0500

    updated kitchen_test

--- scripts/kitchen_test.sh
+++ scripts/kitchen_test.sh
@@ -6,7 +6,7 @@ function kitchen_test {
   echo "Starting Test Kitchen"
 
   kitchen test
-  if [[ ${command_exit_code} -eq 0 ]]; then
+  if [[ ${?} -eq 0 ]]; then
     echo
     echo "Test Kitchen successfully completed ✔"
     echo
