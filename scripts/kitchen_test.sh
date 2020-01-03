cc2fb0a70f9ed811c3957c41bde0390bc233ea16
Patrick Dayton <daytonpa@gmail.com>
2020-01-03 11:40:05 -0500

    updated kitchen_test

--- scripts/kitchen_test.sh
+++ scripts/kitchen_test.sh
@@ -5,9 +5,7 @@ set -oe
 function kitchen_test {
   echo "Starting Test Kitchen"
 
-  local command_output=$(kitchen test 2>&1)
-  local command_exit_code=${?}
-
+  kitchen test
   if [[ ${command_exit_code} -eq 0 ]]; then
     echo
     echo "Test Kitchen successfully completed ✔"
@@ -17,6 +15,4 @@ function kitchen_test {
     echo "Test Kitchen encountered issues ✘"
     echo
   fi
-  echo "${command_output}"
-  exit ${command_exit_code}
 }
